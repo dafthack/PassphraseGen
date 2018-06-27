e5f17d800ad0da3174d72d93883f26bba5832cdd
dafthack <beau@dafthack.com>
2018-06-27 11:31:31 -0400

    Added multi-threading
    Added some multi-threading to speed up list creation.

--- PassphraseGen.ps1
+++ PassphraseGen.ps1
@@ -35,6 +35,14 @@
 
     Use this option to specify a number of lines to randomly select from the wordlist for use within passphrases.
 
+  .PARAMETER Threads
+
+    Use this option to specify a number of threads. Uses 10 by default.
+
+  .PARAMETER SleepTimer
+
+    Time to sleep in between checking if a thread has finished or not.
+
   .EXAMPLE
 
     C:\PS> Invoke-PassphraseGen -FourWords -Wordlist .\lists\top-100-english-words-4-chars-or-more.txt -OutputFile passphrase-list.txt
@@ -50,6 +58,16 @@
     Description
     -----------
     This command will take a list of words and select 25 random lines from it. Then using those 25 words it will generate a passphrase list using four words in each passphrase.
+
+  .EXAMPLE
+
+    C:\PS> Invoke-PassphraseGen -Threads 20 -FourWords -Wordlist .\lists\top-100-english-words-4-chars-or-more.txt -OutputFile passphrase-list.txt
+
+    Description
+    -----------
+    This command will take a list of words and generate a passphrase list using four words in each passphrase. A total of 20 threads will be opened up concurrently.
+
+
 #>
 
 
@@ -73,7 +91,15 @@
 
     [Parameter(Position = 4, Mandatory = $false)]
     [string]
-    $TotalLines = ""
+    $TotalLines = "",
+    
+    [Parameter(Position = 5, Mandatory = $false)]
+    [string]
+    $Threads = 10,
+
+    [Parameter(Position = 6, Mandatory = $false)]
+    [string]
+    $SleepTimer = 500
   )
 
     If (($ThreeWords -ne $true) -and ($FourWords -ne $true))
@@ -144,35 +170,130 @@
     #Now we finally create our passphrase list
     $list = @()
     if ($ThreeWords){
+        $i = 0
+        $out = @()
+        $threadtotalcount = $rawlist.count * $rawlist.count
+        [int]$perarraycount = (Factorial ($rawlistcount - 2))/(Factorial ($rawlistcount - 3))
+        $threadlistempty = @(0) * $perarraycount
         foreach ($c1 in $rawlist) {
-            foreach ($c2 in $rawlist) {
-                foreach ($c3 in $rawlist) {
-                    if (($c1 -ne $c2) -and ($c2 -ne $c3) -and ($c3 -ne $c1))
-                    {
-                        $list += "$c1 $c2 $c3"
+        
+                        While ($(Get-Job -state running).count -ge $threads){
+                        Write-Progress  -Activity "Permutating all teh thingz now..." -Status "Waiting for threads to close" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $threadtotalcount * 100)
+                        Start-Sleep -Milliseconds $SleepTimer
+                        }
+                        
+                        foreach ($c2 in $rawlist){
+                        
+                            While ($(Get-Job -state running).count -ge $threads){
+                            Write-Progress  -Activity "Permutating all teh thingz now..." -Status "Waiting for threads to close" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $threadtotalcount * 100)
+                            Start-Sleep -Milliseconds $SleepTimer
+                            }
+                            $i++
+                            Start-Job -scriptblock $ThreeWordsThread -ArgumentList $c1, $c2, $rawlist | Out-Null
+                            Write-Progress -Activity "Permutating all teh thingz now..." -Status "Starting Threads" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $threadtotalcount * 100)  
+                            ForEach ($thread in $(Get-Job -state completed)){$out += Receive-Job $($thread.name); Remove-Job $($thread.name)}
+                        }
+          }
+ 
+                   While ($(Get-Job -State Running).count -gt 0){
+                        $ThreadsStillRunning = ""
+                        ForEach ($thread  in $(Get-Job -state running)){$ThreadsStillRunning += ", $($thread.name)"}
+                        $ThreadsStillRunning = $ThreadsStillRunning.Substring(2)
+                        Write-Progress  -Activity "Permutating all teh thingz now..." -Status "$($(Get-Job -State Running).count) threads remaining" -CurrentOperation "$ThreadsStillRunning" -PercentComplete ($(Get-Job -State Completed).count / $(Get-Job).count * 100)
+                        Start-Sleep -Milliseconds $SleepTimer
+                        ForEach ($thread in $(Get-Job -state completed)){$out += Receive-Job $($thread.name); Remove-Job $($thread.name)}
+                        Start-Sleep -Milliseconds $SleepTimer
                     }
-                }
-            }
-        } 
+                    Write-Host "[*] Now getting the output from all the threads..." -ForegroundColor Green
+
+                    $out2 = @()
+                    $out2 = ForEach ($job in $(Get-Job)){$out2 += Receive-Job $($job.name); Remove-Job $($job.name)}
+                    $list += $out
+                    $list += $out2
+
     }
     if ($FourWords){
+        $i = 0
+        $out = @()
+        $threadtotalcount = $rawlist.count * $rawlist.count
+        [int]$perarraycount = (Factorial ($rawlistcount - 2))/(Factorial ($rawlistcount - 4))
+        $threadlistempty = @(0) * $perarraycount
         foreach ($c1 in $rawlist) {
-            foreach ($c2 in $rawlist) {
-                foreach ($c3 in $rawlist) {
-                    foreach ($c4 in $rawlist) {
-                        if (($c1 -ne $c2) -and ($c2 -ne $c3) -and ($c3 -ne $c4) -and ($c4 -ne $c1) -and ($c1 -ne $c3) -and ($c2 -ne $c4))
-                        {
-                            $list += "$c1 $c2 $c3 $c4"
+        
+                        While ($(Get-Job -state running).count -ge $threads){
+                        Write-Progress  -Activity "Permutating all teh thingz now..." -Status "Waiting for threads to close" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $threadtotalcount * 100)
+                        Start-Sleep -Milliseconds $SleepTimer
                         }
+                        
+                        foreach ($c2 in $rawlist){
+                        
+                            While ($(Get-Job -state running).count -ge $threads){
+                            Write-Progress  -Activity "Permutating all teh thingz now..." -Status "Waiting for threads to close" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $threadtotalcount * 100)
+                            Start-Sleep -Milliseconds $SleepTimer
+                            }
+                            $i++
+                            Start-Job -scriptblock $FourWordsThread -ArgumentList $c1, $c2, $rawlist | Out-Null
+                            Write-Progress -Activity "Permutating all teh thingz now..." -Status "Starting Threads" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $threadtotalcount * 100)  
+                            ForEach ($thread in $(Get-Job -state completed)){$out += Receive-Job $($thread.name); Remove-Job $($thread.name)}
+                        }
+          }
+ 
+                   While ($(Get-Job -State Running).count -gt 0){
+                        $ThreadsStillRunning = ""
+                        ForEach ($thread  in $(Get-Job -state running)){$ThreadsStillRunning += ", $($thread.name)"}
+                        $ThreadsStillRunning = $ThreadsStillRunning.Substring(2)
+                        Write-Progress  -Activity "Permutating all teh thingz now..." -Status "$($(Get-Job -State Running).count) threads remaining" -CurrentOperation "$ThreadsStillRunning" -PercentComplete ($(Get-Job -State Completed).count / $(Get-Job).count * 100)
+                        Start-Sleep -Milliseconds $SleepTimer
+                        ForEach ($thread in $(Get-Job -state completed)){$out += Receive-Job $($thread.name); Remove-Job $($thread.name)}
+                        Start-Sleep -Milliseconds $SleepTimer
                     }
-                }
-            }
-        } 
+                    Write-Host "[*] Now getting the output from all the threads..." -ForegroundColor Green
+
+                    $out2 = @()
+                    $out2 = ForEach ($job in $(Get-Job)){$out2 += Receive-Job $($job.name); Remove-Job $($job.name)}
+                    $list += $out
+                    $list += $out2
+
     }
     Write-Host "[*] Writing passphrases to $OutputFile..." -ForegroundColor yellow
     
     #Write the passphrase list to a file
     Out-File -FilePath $OutputFile -InputObject $list -Encoding ascii
+}
+
+$FourWordsThread = {
+    Param($c1, $c2, $rawlist)#, $threadlistempty)
+    #$threadlist = $threadlistempty
+    $threadlist = @()
+            #$k = 0
+            foreach ($c3 in $rawlist) {
+                foreach ($c4 in $rawlist) {
+                    
+                    if (($c1 -ne $c2) -and ($c2 -ne $c3) -and ($c3 -ne $c4) -and ($c4 -ne $c1) -and ($c1 -ne $c3) -and ($c2 -ne $c4))
+                    {
+                        $threadlist += "$c1 $c2 $c3 $c4"
+                        #$threadlist[$k] = "$c1 $c2 $c3 $c4"
+                        #$k++
+                    }
+                }
+            }       
+
+    #if ($threadlist[0] -ne 0) {
+    $threadlist
+    #}
+}
+
+
+$ThreeWordsThread = {
+    Param($c1, $c2, $rawlist)
+    $threadlist = @()
+        foreach ($c3 in $rawlist) {
+            if (($c1 -ne $c2) -and ($c2 -ne $c3) -and ($c3 -ne $c1))
+            {
+                $threadlist += "$c1 $c2 $c3"
+            }
+        }
+    $threadlist
 }
 
 function Factorial ([bigint]$x) {
